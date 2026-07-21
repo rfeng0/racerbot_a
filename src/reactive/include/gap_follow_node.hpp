ca944ce2df81d125394ebac1de8f169222b78703
“Milad244” <145618774+Milad244@users.noreply.github.com>
2026-07-20 22:09:55 -0700

    Add basic gap_follow_node driving to best gap

--- src/reactive/include/gap_follow_node.hpp
+++ src/reactive/include/gap_follow_node.hpp
@@ -2,6 +2,7 @@
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
+#include "reactive/msg/gap.hpp"
 
 class GapFollowNode : public rclcpp::Node
 {
@@ -10,4 +11,13 @@ public:
 
 private:
     rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_pub_;
+    rclcpp::Subscription<reactive::msg::Gap>::SharedPtr gap_sub_;
+
+    /// @brief Callback invoked each time we find a valid gap from the laser scan.
+    /// @param gap_msg Shared pointer to the incoming Gap message.
+    void gap_callback(const reactive::msg::Gap::ConstSharedPtr gap_msg);
+
+    /// @brief Baseline method that publishes to drive, driving to the best gap found in the gap_msg.
+    /// @param gap_msg Shared pointer to the incoming Gap message.
+    void drive_best_point(const reactive::msg::Gap::ConstSharedPtr gap_msg);
 };
--- src/reactive/src/gap_follow_node.cpp
+++ src/reactive/src/gap_follow_node.cpp
@@ -4,7 +4,35 @@ GapFollowNode::GapFollowNode() : Node("gap_follow_node")
 {
     RCLCPP_INFO(this->get_logger(), "Gap Follow node started");
 
-    this->drive_pub_ = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>("drive", 10);
+    drive_pub_ = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>("drive", 10);
+
+    gap_sub_ = this->create_subscription<reactive::msg::Gap>(
+    "gap", 10,
+    std::bind(&GapFollowNode::gap_callback, this, std::placeholders::_1));
+}
+
+void GapFollowNode::gap_callback(const reactive::msg::Gap::ConstSharedPtr gap_msg)
+{
+    drive_best_point(gap_msg);
+    
+}
+
+void GapFollowNode::drive_best_point(const reactive::msg::Gap::ConstSharedPtr gap_msg)
+{
+    // Speed depends on target point range
+    float velocity;
+    float target_range = gap_msg->target_range;
+    if (target_range > 2) velocity = 2;
+    else if (target_range > 1) velocity = 1.5;
+    else if (target_range > 0.5) velocity = 1;
+    else velocity = 0.5;
+
+    // Publishing to drive
+    ackermann_msgs::msg::AckermannDriveStamped drive_msg;
+    drive_msg.header.stamp = this->now();
+    drive_msg.drive.steering_angle = gap_msg->target_angle;
+    drive_msg.drive.speed = velocity;
+    drive_pub_->publish(drive_msg);
 }
 
 int main(int argc, char **argv)
